780e5ffd8defb0393d2f0a48d730f87fb717b892
Rocket-Fish <rocket.fish@mail.com>
2019-01-23 15:27:14 -0500

    setup backend login section separate from the SPA section

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,50 @@
 
 /*
 * Give Vue js Route control for all default cases
-* the '^(?!(api|secret)).*$' part separates out any routes which start with
+* For example '^(?!(api|secret)).*$' separates out any routes which start with
 * /api/ or /secret/
 * this is so we can have an api and semi-secret access for me to make logins with
+* We will however, be building this from a constant array for better scalability
 */
-Route::get('/{any}', 'PageController@index')->where('any', '^(?!(api|secret)).*$');
 
-// TODO: secret routing goes here
+// obtain routes from constants
+$routes = Config::get('constants.routes');
+// calculate routes count
+$routesCount = count($routes);
+// define output text
+$outputText = "";
+
+// counter, because we don't need to add a | after the last item
+$i = 0;
+// iterate through the array
+foreach ($routes as $r){
+  $i++;
+
+  // append array content
+  $outputText .= $r;
+  // add | if its not the last item
+  if($i < $routesCount) {
+    $outputText .= '|';
+  }
+}
+
+//setup the route
+Route::get('/{any}', 'PageController@index')->where('any', '^(?!('.$outputText.')).*$');
+
+/* secret routing goes here
+*
+* secretRoute variable stores our route
+*
+*/
+$secretRoute = '/' . Config::get('constants.routes.secret');
+
+
+// add prefix to the magic laravel facad
+Route::group(['prefix' => $secretRoute], function () {
+  // magic routing by default laravel
+  Auth::routes();
+  // secret home
+  Route::get('/', 'HomeController@index')->name('home');
+});
 
 // TODO: badkend api routing goes here
